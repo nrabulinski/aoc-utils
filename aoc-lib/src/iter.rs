cd1d212e6d48e50e9fb5a4e0daabca4566d45d55
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-11-30 23:20:25 +0100

    iter: add chain_sorted

--- aoc-lib/src/iter.rs
+++ aoc-lib/src/iter.rs
@@ -1,4 +1,7 @@
-use std::mem::MaybeUninit;
+use std::{
+	iter::{Fuse, FusedIterator, Peekable},
+	mem::MaybeUninit,
+};
 
 pub trait IterExt: Iterator {
 	fn peekable2(self) -> Peek2<Self>
@@ -23,12 +26,41 @@ pub trait IterExt: Iterator {
 		curr == n
 	}
 
-	fn arr_chunks<const N: usize>(self) -> Chunks<Self, N>
+	/// Iterator::array_chunks, but stable
+	fn arr_chunks<const N: usize>(self) -> Chunks<Fuse<Self>, N>
 	where
 		Self: Sized,
 	{
 		assert!(N != 0, "chunk size must be non-zero");
-		Chunks { iter: self }
+		Chunks { iter: self.fuse() }
+	}
+
+	/// Chains two iterators, but always returns the smaller element of the two.
+	///
+	/// This means that if the iterators that are being chained are sorted,
+	/// the resulting iterator will be sorted as well.
+	///
+	/// # Examples
+	/// ```
+	/// use aoc_lib::iter::IterExt;
+	///
+	/// let a = [1, 3, 5];
+	/// let b = [2, 4, 6];
+	/// let sorted_iter: Vec<_> = a.into_iter().chain_sorted(b).collect();
+	///
+	/// assert_eq!(sorted_iter, [1, 2, 3, 4, 5, 6]);
+	/// ```
+	fn chain_sorted<U>(self, other: U) -> ChainSorted<Self, U::IntoIter>
+	where
+		Self: Sized,
+		// TODO: Could I get away with PartialOrd?
+		Self::Item: Ord,
+		U: IntoIterator<Item = Self::Item>,
+	{
+		ChainSorted {
+			a: self.peekable(),
+			b: other.into_iter().peekable(),
+		}
 	}
 }
 
@@ -88,11 +120,11 @@ impl<I: Iterator> Iterator for Peek2<I> {
 	}
 }
 
-pub struct Chunks<I: Iterator, const N: usize> {
+pub struct Chunks<I: FusedIterator, const N: usize> {
 	iter: I,
 }
 
-impl<I: Iterator, const N: usize> Iterator for Chunks<I, N> {
+impl<I: FusedIterator, const N: usize> Iterator for Chunks<I, N> {
 	type Item = [I::Item; N];
 
 	fn next(&mut self) -> Option<Self::Item> {
@@ -110,5 +142,41 @@ impl<I: Iterator, const N: usize> Iterator for Chunks<I, N> {
 
 		// SAFETY: We know we have filled in all the array elements
 		Some(unsafe { res.assume_init() })
+	}
+}
+
+impl<I: FusedIterator, const N: usize> FusedIterator for Chunks<I, N> {}
+
+pub struct ChainSorted<I1, I2>
+where
+	I1: Iterator,
+	I1::Item: Ord,
+	I2: Iterator<Item = I1::Item>,
+{
+	a: Peekable<I1>,
+	b: Peekable<I2>,
+}
+
+impl<I1, I2> Iterator for ChainSorted<I1, I2>
+where
+	I1: Iterator,
+	I1::Item: Ord,
+	I2: Iterator<Item = I1::Item>,
+{
+	type Item = I1::Item;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		match (self.a.peek(), self.b.peek()) {
+			(Some(a), Some(b)) => {
+				if a < b {
+					self.a.next()
+				} else {
+					self.b.next()
+				}
+			}
+			(Some(_), None) => self.a.next(),
+			(None, Some(_)) => self.b.next(),
+			_ => None,
+		}
 	}
 }
